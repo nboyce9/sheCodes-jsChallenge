62db7ffcf84b91b80c90f22a88536d7b1f51e2b4
Boyce Nobukho <145935259+nboyce9@users.noreply.github.com>
2025-01-19 14:33:05 +0000

    change city

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 let now = new Date();
 
-let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 let day = days[now.getDay()];
 let hour = now.getHours();
@@ -9,4 +17,12 @@ let minutes = now.getMinutes();
 let dayTime = document.querySelector("#date-time");
 dayTime.innerHTML = `${day} ${hour}:${minutes}`;
 
+function displayCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector(".search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = cityInput.value;
+}
 
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", displayCity);
